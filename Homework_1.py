2a3494a59dc9a4289da4b041ead4040d5f019fc6
Bogdan <sirbony95@gmail.com>
2023-02-21 21:15:10 +0200

    Created simple calculator

--- Homework_1.py
+++ Homework_1.py
@@ -1,2 +1,39 @@
 
-print("Hello")
+while True:
+ try:
+  value_digit = float(input("Add a number:"))
+  break
+ except:
+  print("Type just a number")
+
+operation = input("Please choose operation:\n'+'\n'-'\n'*'\n'/'\n ")
+
+while True:
+ try:
+  value_another_digit = float(input("Add another number:"))
+  break
+ except:
+  print("Type just a number")
+
+if operation == '+':
+ print("Your answer:"'\n{} + {} ='.format(value_digit,value_another_digit), value_digit + value_another_digit)
+elif operation == '-':
+ print("Your answer:"'\n{} - {} ='.format(value_digit,value_another_digit), value_digit - value_another_digit)
+elif operation == '*':
+ print("Your answer:"'\n{} * {} ='.format(value_digit,value_another_digit), value_digit * value_another_digit)
+elif operation == '/':
+ if value_another_digit == 0:
+  while value_another_digit == 0:
+   try:
+     value_another_digit = float(input("Add another number:"))
+   except:
+    print('Try again')
+ print("Your answer:"'\n{} / {} ='.format(value_digit, value_another_digit), value_digit / value_another_digit)
+
+
+
+
+
+
+
+
